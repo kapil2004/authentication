f534037be3356b892a6ffe9e4f014921859cbc18
Kapil Gupta <kapil2004@gmail.com>
2018-05-22 13:12:48 -0400

    make authenticated request to root route

--- server/services/passport.js
+++ server/services/passport.js
@@ -4,7 +4,10 @@ const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 
-const jwtOptions = {};
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+  secretOrKey: config.secret
+};
 
 
 // payload is decoded jwt token
@@ -19,3 +22,5 @@ const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
     }
   });
 });
+
+passport.use(jwtLogin);
