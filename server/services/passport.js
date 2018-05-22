644b0c639316bb95e01fcf16bb46e326d7e8fa0f
Kapil Gupta <kapil2004@gmail.com>
2018-05-22 12:51:39 -0400

    get passport, passwort-jwt, initialize jwtstrategy

---
+++ server/services/passport.js
@@ -0,0 +1,21 @@
+const passport = require('passport');
+const User = require('../models/user');
+const config = require('../config');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+const jwtOptions = {};
+
+
+// payload is decoded jwt token
+const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
+  User.findById(payload.sub, function(err, user) {
+    if (err) { return done(err, false); }
+
+    if(user) {
+      done(null, user);
+    } else {
+      done(null, false);
+    }
+  });
+});
