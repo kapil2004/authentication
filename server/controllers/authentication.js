8652f9efc5ace549d5d1539738395d53cedb890d
Kapil Gupta <kapil2004@gmail.com>
2018-05-22 15:42:33 -0400

    add local strategy for signin with email, pw

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -7,6 +7,11 @@ function tokenForUser(user) {
   return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
 }
 
+
+exports.signin = function(req, res, next) {
+  res.send({ token: tokenForUser(req.user) });
+};
+
 exports.signup = function(req, res, next) {
   // see if user exists
   const email = req.body.email;
--- server/services/passport.js
+++ server/services/passport.js
@@ -3,7 +3,27 @@ const User = require('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
 
+// local login with email/password
+const localOptions = { usernameField: 'email' };
+
+const localLogin = new LocalStrategy(localOptions, function(email, password, done) {
+  User.findOne({ email: email }, function(err, user) {
+    if (err) { return done(err); }
+    if (!user) { return done(null, false); }
+
+    user.comparePassword(password, function(err, isMatch) {
+      if (err) { return done(err); }
+
+      if (!isMatch) { return done(null, false); }
+
+      return done(null, user);
+    });
+  });
+});
+
+// jwt login/authentication
 const jwtOptions = {
   jwtFromRequest: ExtractJwt.fromHeader('authorization'),
   secretOrKey: config.secret
@@ -24,3 +44,4 @@ const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
 });
 
 passport.use(jwtLogin);
+passport.use(localLogin);
